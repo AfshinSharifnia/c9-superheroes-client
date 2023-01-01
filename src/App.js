e7d92110bbc084231b22072a34e98dd633a1d262
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-01 11:18:12 -0700

    deleted default files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+const [count2, setCount2]= useState(0)
+
+
+  const [employeeName, SetEmployeeName] = useState("Bob");
+
+  function createNewEmployee() {
+
+    SetEmployeeName("Joe")
+  }
+
+
+
+
   return (
+
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {employeeName} added
+<br/>
+<br/>
+        <button onClick={createNewEmployee} className="App-header">
+          Create New Employee
+        </button>
+
+        <select>
+          <option> Create Employee</option>
+          <option> UPdate Employee</option>
+          <option> Read Employee</option>
+          <option> Delete Employee</option>
+        </select>
+
       </header>
     </div>
   );
