02068b5df177391560e14f340a5ef9988be84c31
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-02 15:06:22 -0700

    Added new component

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,40 +1,27 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import { useState } from "react";
 
 const Content = () => {
-  
+
+//React hook
+const [name, setName] = useState("");
+
   const handleNameChange = () => {
+
     const names = ["Bob", "Kevin", "Dave"];
 
     const int = Math.floor(Math.random() * 3);
 
-    return names[int];
+    setName(names[int])
+
+    // return names[int];
   };
 
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <p >Hello {name}</p>
+
+<button onClick={handleNameChange}>Change Name </button>
+
     </main>
   );
 };
