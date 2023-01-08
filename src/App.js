27d28dc8c4dc042bc6075ad07a2659b8fc5c2e78
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-07 18:32:18 -0700

    Added new components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
 function App() {
-
-
-
-  
   return (
     <div className="App">
       <Header />
+      <Content />
       <Footer />
-      <Content/>
     </div>
   );
 }
